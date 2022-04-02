12ae9a9f8b2141b5140f2ae7f12fcc16c86dc882
gabrymattioli <gabrymattioli@gmail.com>
2022-04-02 19:40:49 +0200

    Translated book_management decorators documentation

--- book_management/decorators.py
+++ book_management/decorators.py
@@ -5,7 +5,7 @@ from user_management.models import ProfileBook
 
 def profile_book_exists_only(func):
     """
-    Decoratore per negare l'accesso a utenti che NON hanno completato il proprio profilo.
+    Decorator to deny the access to users whom have not completed their profile.
     """
     def check_and_call(request, *args, **kwargs):
         if not ProfileBook.objects.filter(book_id=kwargs['pk'], profile_owner_id=request.user.profile.pk).exists():
