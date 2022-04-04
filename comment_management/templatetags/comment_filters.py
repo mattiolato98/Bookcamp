238467568161b42cf4db5cf0c61957bd4d130ed2
gabrymattioli <gabrymattioli@gmail.com>
2022-04-04 18:30:00 +0200

    Translated comment_management templatetags documentation

--- comment_management/templatetags/comment_filters.py
+++ comment_management/templatetags/comment_filters.py
@@ -7,10 +7,10 @@ from comment_management.models import Like, Bookmark, Comment
 @register.filter
 def user_likes_topic(topic, user):
     """
-    Verifica se all'utente piace il topic.
-    :param topic: Oggetto Topic.
-    :param user: Oggetto PlatformUser.
-    :return: True se l'utente ha messo like al topic, False altrimenti.
+    Check if the user likes the Topic.
+    :param topic: Topic object.
+    :param user: PlatformUser object.
+    :return: True if the user likes the Topic, False otherwise.
     """
     return Like.objects.filter(topic_id=topic.pk, user_owner_id=user.pk).exists()
 
@@ -18,10 +18,10 @@ def user_likes_topic(topic, user):
 @register.filter
 def user_saved_topic(topic, user):
     """
-    Verifica se l'utente ha salvato il topic.
-    :param topic: Oggetto Topic.
-    :param user: Oggetto PlatformUser.
-    :return: True se l'utente ha salvato il topic, False altrimenti.
+    Check if the user has saved the Topic as a Bookmark.
+    :param topic: Topic object.
+    :param user: PlatformUser object.
+    :return: True if the user has saved the Topic, False otherwise.
     """
     return Bookmark.objects.filter(topic_id=topic.pk, user_owner_id=user.pk).exists()
 
@@ -29,10 +29,10 @@ def user_saved_topic(topic, user):
 @register.filter
 def filter_by_user(objects, user):
     """
-    Filtra gli oggetti di proprietà dell'utente.
-    :param objects: Oggetti generici.
-    :param user: Oggetto PlatformUser.
-    :return: Lista di oggetti filtrata, contenente solo quelli di proprietà dell'utente
+    Filters objects owned by current user.
+    :param objects: Generic objects.
+    :param user: PlatformUser object.
+    :return: Filtered object list, containing only user owned objects.
     """
     return objects.filter(user_owner=user)
 
@@ -40,10 +40,10 @@ def filter_by_user(objects, user):
 @register.filter
 def filter_by_user_count(objects, user):
     """
-    Filtra gli oggetti di proprietà dell'utente e li conta.
-    :param objects: Oggetti generici.
-    :param user: Oggetto PlatformUser.
-    :return: Numero di oggetti nella lista filtrata, contenente solo oggetti di proprietà dell'utente.
+    Count user owned objects.
+    :param objects: Generic objects.
+    :param user: PlatformUser object.
+    :return: User owned objects number.
     """
     return objects.filter(user_owner=user).count()
 
@@ -51,10 +51,10 @@ def filter_by_user_count(objects, user):
 @register.filter
 def truncate_html_chars(text, chars):
     """
-    Tronca un testo renderizzato come html. Evita che un tag HTML venga troncato in maniera errata.
-    :param text: Testo completo.
-    :param chars: Numero di caratteri a cui troncare il testo.
-    :return: Testo troncato con salvaguardia dei tag HTML.
+    Truncates an HTML rendered text. Prevents an HTML tag from being truncated incorrectly.
+    :param text: Complete text.
+    :param chars: Characters to truncate the text to.
+    :return: Truncated text.
     """
     truncator = Truncator(text)
     return truncator.chars(chars, html=True)
