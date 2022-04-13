f8fe959c18c3dd1cf14c98b899f6746fc24a12a6
gabrymattioli <gabrymattioli@gmail.com>
2022-04-13 15:00:11 +0200

    Translated user_management templatetags documentation

--- user_management/templatetags/user_filters.py
+++ user_management/templatetags/user_filters.py
@@ -4,10 +4,10 @@ from django.template.defaulttags import register
 @register.filter
 def follow(user_following, user_followed):
     """
-    Verifica se utente ne segue un altro.
-    :param user_following: Utente che segue/non segue.
-    :param user_followed: Utente seguito/non seguito.
-    :return: True se lo user_following segue lo user_followed, False altrimenti.
+    Checks if a user follows another one.
+    :param user_following: User that follow (or not).
+    :param user_followed: User followed (or not).
+    :return: True if user_following follow user_followed, False otherwise.
     """
     return user_following in user_followed.profile.followers.all()
 
